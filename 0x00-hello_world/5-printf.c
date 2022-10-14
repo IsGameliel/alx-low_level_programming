4f221cc4484aab5e0a510c6866a2abe9251d7fb1
IsGameliel <simvicpro@gmail.com>
2022-10-13 19:18:01 -0700

    check

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main prints quote
+ * main - this serves a the program's entery point
  *
- * Return  zero as success
- *
+ * returns zero by default
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-	return (0)
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
