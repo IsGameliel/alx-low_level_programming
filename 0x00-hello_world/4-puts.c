34515f3c1f79b3f936e7fe7608953f2aed241754
IsGameliel <simvicpro@gmail.com>
2022-10-13 18:38:08 -0700

    reallly a multilingual puzzle

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - serves as program's entry point
+ *
+ * Return: By default it will be zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
