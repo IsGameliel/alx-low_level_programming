9e314d3ec4fe54603fa2caa74a970f3954f616ae
IsGameliel <simvicpro@gmail.com>
2022-10-13 19:35:27 -0700

    unix is really basic

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - Prints quote to the standard error
+ *
+ * Return: zero on success
+ *
+ **/
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
