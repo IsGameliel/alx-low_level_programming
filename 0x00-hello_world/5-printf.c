d52f2ae604a29953925177e54dc62696606c6089
IsGameliel <simvicpro@gmail.com>
2022-10-13 18:58:43 -0700

    print sizes and printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main prints quote
+ *
+ * Return  zero as success
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0)
+}
