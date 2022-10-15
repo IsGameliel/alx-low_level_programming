7ef8356e710ba8056835502af3c11b4d11028f40
IsGameliel <simvicpro@gmail.com>
2022-10-15 06:38:36 +0200

    conditional statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,10 +14,10 @@
  * */
 int main(void)
 {
-	/**
-	* variable n has been declared
-	* coding then assigns the random value.
-	*/
+/**
+ *  variable n has been declared
+ *  coding then assigns the random value.
+ * */
 		int n;
 
 		srand(time(0));
