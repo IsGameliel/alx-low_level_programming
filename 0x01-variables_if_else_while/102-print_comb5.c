ce358ef3a2e7a6b838bb30954cca313d81a5b9b2
IsGameliel <simvicpro@gmail.com>
2022-10-15 23:57:31 +0200

    advance task unique prints

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Prints combinations of 00-99
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints all special combinations of 2
+ * digit numbers
+ *
+ * Return: 0 for success
+*/
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = 0; j <= 99; j++)
+		{
+			if (i < j)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
