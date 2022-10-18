fd1bcc8b1c26d597748f06741ac1ef2f5974f622
IsGameliel <simvicpro@gmail.com>
2022-10-18 20:21:14 +0200

    -putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "stdio.h"
+
+/**
+ * main - Prints Holberton
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints Holberton w/out stdio
+ *
+ * Return: 0 for success
+*/
+int main()
+{
+	printf("_putchar");
+    printf("\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+functions in c programming 
