5bbfab7fc22ebf734c2d97b4d474edc72b0353ae
IsGameliel <simvicpro@gmail.com>
2022-10-20 22:27:08 +0200

    more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0-9 without 2 and 4
+ *
+ * Return: nothing
+ **/
+
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if ((i == '2') || (i == '4'))
+			i++;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
