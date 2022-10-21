d74f01678afe8d8c1cc2ecd951cf154fec371e0f
IsGameliel <simvicpro@gmail.com>
2022-10-21 02:14:40 +0200

    numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,21 +6,21 @@
  */
 void print_number(int n)
 {
-	unsigned int x1;
+	unsigned int n1;
 
 	if (n < 0)
 	{
-		x1 = -n;
+		n1 = -n;
 		_putchar('-');
 	} else
 	{
-		x1 = n;
+		n1 = n;
 	}
 
-	if (x1 / 10)
+	if (n1 / 10)
 	{
-		print_number(x1 / 10);
+		print_number(n1 / 10);
 	}
 
-	_putchar((x1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
