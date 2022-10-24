f323c5bf9a1a4b4c294b212d6eff443ede8af13a
IsGameliel <simvicpro@gmail.com>
2022-10-24 18:27:56 +0200

    prints n digits of array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,10 +15,10 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar("%d", a[i]);
+		printf("%d", a[i]);
 		if (i != n - 1)
-			_putchar(", ");
+			printf(", ");
 	}
 
-	_putchar("\n");
+	printf("\n");
 }
