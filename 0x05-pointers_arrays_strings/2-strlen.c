02da8fd872a3de9e22d825378450f8f648ebd808
IsGameliel <simvicpro@gmail.com>
2022-11-03 03:02:53 +0200

    few corrections

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,7 +3,7 @@
 /**
  * _strlen - returns the length of the string
  * @s: input string to count
- * Description: returns the length of a given string
+ * Description: this will return the length of a given string
  * Return: length of string as int
  **/
 
--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
-nt main(void)
+int main(void)
 {
-
 	int n;
 	int a[5];
-	int *p;
 
 	a[2] = 1024;
-	p = &n;
+
 	/*
 	 * write your line of code here...
 	 * Remember:
@@ -17,8 +15,8 @@ nt main(void)
 	 * - only one statement
 	 * - you are not allowed to code anything else than this line of code
 	 */
-	p[5] = 98;
-	/** ...so that this prints 98\n */
+	*(&n + 3) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
