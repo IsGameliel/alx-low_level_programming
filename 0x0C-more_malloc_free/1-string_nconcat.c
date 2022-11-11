ef4e2421e5c5ca7d85a5a046b88da30247079769
isgameliel <simvicpro@gmail.com>
2022-11-11 03:51:07 +0200

    concatenate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,54 +1,46 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-/**
- * _strlen - returns the length of the string
- * @s: input string to count
- * Description: returns the length of a given string
- * Return: length of string as int
- **/
-int _strlen(char *s)
-{
-	unsigned int i;
 
-	if (s == NULL)
-		return (1);
-	for (i = 0; s[i] != '\0'; i++)
-		;
-	return (i + 1);
-}
 /**
- * string_nconcat - concatenate n chars of second string to first
- * @s1: first string
- * @s2: second string
- * @n: number of chars to concatenate
- * Description: concatenate two strings, return NULL if fails
- * Return: pointer to string, NULL if fails
- **/
+ * string_nconcat - a function that concatenates two strings.
+ *
+ * @s1: first char
+ * @s2: secound char
+ * @n: unsigned int
+ *
+ * Return: If the function fails, it should return NULL
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *dest;
-	unsigned int size1, size2, i, j;
+	unsigned int x, y, z;
+	char *s;
 
-	size1 = _strlen(s1);
-	size2 = _strlen(s2);
-	if (n > size2 - 1)
-		n = size2 - 1;
-
-	dest = (char *)malloc((size1 + n) * sizeof(char));
-	if (dest == NULL)
-		return (NULL);
-	if (size1 == 1 && size2 == 1)
-		return (NULL);
-
-	for (i = 0; i < size1 - 1; i++)
-		dest[i] = s1[i];
-
-	for (j = 0; j <= n; j++)
+	if (s1 == NULL)
 	{
-		dest[i + j] = s2[j];
+		x = 0;
 	}
-	dest[i + j] = '\0';
-
-	return (dest);
+	else
+	{
+		for (x = 0; s1[x]; ++x)
+		;
+	}
+	if (s2 == NULL)
+	{
+		y = 0;
+	}
+	else
+	{
+		for (y = 0; s2[y]; ++y)
+		;
+	}
+	if (y > n)
+		y = n;
+	s = malloc(sizeof(char) * (x + y + 1));
+	if (s == NULL)
+		return (NULL);
+	for (z = 0; z < x; z++)
+		s[z] = s1[z];
+	for (z = 0; z < y; z++)
+		s[z + x] = s2[z];
+	s[x + y] = '\0';
+	return (s);
 }
